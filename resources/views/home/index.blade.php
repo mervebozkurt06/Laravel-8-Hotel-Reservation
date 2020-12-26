1a930ef3374ee2f07946b8e6c760b7ca1d1d4b18
mervebozkurt06 <mervebzkrts@gmail.com>
2020-12-26 16:04:15 +0300

    Index updated

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('title','Laravel Hotel Reservation')
 @section('description')
-    Click For Online Hotel Reservation
+    Click -> For Online Hotel Reservation
 @endsection
 
 @section('keywords','Airbnb','Hotel','hostel','vacation')
