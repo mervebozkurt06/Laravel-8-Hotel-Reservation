2964f94ae853a33baf533e94a66265aecd66f44f
mervebozkurt06 <mervebzkrts@gmail.com>
2020-12-27 16:19:51 +0300

    check availability added from another template

--- resources/views/home/_slider.blade.php
+++ resources/views/home/_slider.blade.php
@@ -1,6 +1,7 @@
 <div class="container">
     <div class="row">
-        <div class="col-md-12">
+
+        <div class="col-md-12" style="height: 490px"> <? css changed in style.css file 114 line ?>
             <header class="slider-main">
                 <div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-ride="carousel">
                     <ol class="carousel-indicators">
@@ -42,5 +43,132 @@
                 </div>
             </header>
         </div>
+
+
+        <? HOTEL SEARCH PART
+        check availability
+        ?>
+
+
+            <div class="container ie-h-align-center-fix">
+                <div class="row">
+                    <div class="col-xs-12 ml-auto mr-auto ie-container-width-fix">
+                        <form action="index.html" method="get" class="tm-search-form tm-section-pad-2">
+                            <div class="form-row tm-search-form-row">
+                                <div class="form-group tm-form-element tm-form-element-100">
+                                    <i class="fa fa-map-marker fa-2x tm-form-element-icon"></i>
+                                    <input name="city" type="text" class="form-control" id="inputCity" placeholder="Type your destination...">
+                                </div>
+                                <div class="form-group tm-form-element tm-form-element-50">
+                                    <i class="fa fa-calendar fa-2x tm-form-element-icon"></i>
+                                    <input name="check-in" type="text" class="form-control" id="inputCheckIn" placeholder="Check In">
+                                    <div class="qs-datepicker qs-hidden" style="top:45.20000076293945px;left:5px;">
+                                        <div class="qs-controls">
+                                            <div class="qs-arrow qs-left"></div>
+                                            <div class="qs-month-year">
+                                                <span class="qs-month">December</span>
+                                                <span class="qs-year">2020</span>
+                                            </div>
+                                            <div class="qs-arrow qs-right"></div>
+                                        </div>
+                                        <div class="qs-squares"><div class="qs-square qs-day">Sun</div><div class="qs-square qs-day">Mon</div><div class="qs-square qs-day">Tue</div><div class="qs-square qs-day">Wed</div><div class="qs-square qs-day">Thu</div><div class="qs-square qs-day">Fri</div><div class="qs-square qs-day">Sat</div><div class="qs-square qs-num Sun qs-empty"></div><div class="qs-square qs-num Mon qs-empty"></div><div class="qs-square qs-num Tue "><span class="qs-num">1</span></div><div class="qs-square qs-num Wed "><span class="qs-num">2</span></div><div class="qs-square qs-num Thu "><span class="qs-num">3</span></div><div class="qs-square qs-num Fri "><span class="qs-num">4</span></div><div class="qs-square qs-num Sat "><span class="qs-num">5</span></div><div class="qs-square qs-num Sun "><span class="qs-num">6</span></div><div class="qs-square qs-num Mon "><span class="qs-num">7</span></div><div class="qs-square qs-num Tue "><span class="qs-num">8</span></div><div class="qs-square qs-num Wed "><span class="qs-num">9</span></div><div class="qs-square qs-num Thu "><span class="qs-num">10</span></div><div class="qs-square qs-num Fri "><span class="qs-num">11</span></div><div class="qs-square qs-num Sat "><span class="qs-num">12</span></div><div class="qs-square qs-num Sun "><span class="qs-num">13</span></div><div class="qs-square qs-num Mon "><span class="qs-num">14</span></div><div class="qs-square qs-num Tue "><span class="qs-num">15</span></div><div class="qs-square qs-num Wed "><span class="qs-num">16</span></div><div class="qs-square qs-num Thu "><span class="qs-num">17</span></div><div class="qs-square qs-num Fri "><span class="qs-num">18</span></div><div class="qs-square qs-num Sat "><span class="qs-num">19</span></div><div class="qs-square qs-num Sun "><span class="qs-num">20</span></div><div class="qs-square qs-num Mon "><span class="qs-num">21</span></div><div class="qs-square qs-num Tue "><span class="qs-num">22</span></div><div class="qs-square qs-num Wed "><span class="qs-num">23</span></div><div class="qs-square qs-num Thu "><span class="qs-num">24</span></div><div class="qs-square qs-num Fri "><span class="qs-num">25</span></div><div class="qs-square qs-num Sat qs-current"><span class="qs-num">26</span></div><div class="qs-square qs-num Sun "><span class="qs-num">27</span></div><div class="qs-square qs-num Mon "><span class="qs-num">28</span></div><div class="qs-square qs-num Tue "><span class="qs-num">29</span></div><div class="qs-square qs-num Wed "><span class="qs-num">30</span></div><div class="qs-square qs-num Thu "><span class="qs-num">31</span></div><div class="qs-square qs-num Fri qs-empty"></div><div class="qs-square qs-num Sat qs-empty"></div></div>
+                                        <div class="qs-overlay qs-hidden">
+                                            <div class="qs-close">✕</div>
+                                            <input type="number" class="qs-overlay-year" placeholder="4-digit year">
+                                            <div class="qs-submit qs-disabled">Submit</div>
+                                        </div>
+                                    </div></div>
+                                <div class="form-group tm-form-element tm-form-element-50">
+                                    <i class="fa fa-calendar fa-2x tm-form-element-icon"></i>
+                                    <input name="check-out" type="text" class="form-control" id="inputCheckOut" placeholder="Check Out">
+                                    <div class="qs-datepicker qs-hidden" style="top:45.20000076293945px;left:5px;">
+                                        <div class="qs-controls">
+                                            <div class="qs-arrow qs-left"></div>
+                                            <div class="qs-month-year">
+                                                <span class="qs-month">December</span>
+                                                <span class="qs-year">2020</span>
+                                            </div>
+                                            <div class="qs-arrow qs-right"></div>
+                                        </div>
+                                        <div class="qs-squares"><div class="qs-square qs-day">Sun</div><div class="qs-square qs-day">Mon</div><div class="qs-square qs-day">Tue</div><div class="qs-square qs-day">Wed</div><div class="qs-square qs-day">Thu</div><div class="qs-square qs-day">Fri</div><div class="qs-square qs-day">Sat</div><div class="qs-square qs-num Sun qs-empty"></div><div class="qs-square qs-num Mon qs-empty"></div><div class="qs-square qs-num Tue "><span class="qs-num">1</span></div><div class="qs-square qs-num Wed "><span class="qs-num">2</span></div><div class="qs-square qs-num Thu qs-active"><span class="qs-num">3</span></div><div class="qs-square qs-num Fri "><span class="qs-num">4</span></div><div class="qs-square qs-num Sat "><span class="qs-num">5</span></div><div class="qs-square qs-num Sun "><span class="qs-num">6</span></div><div class="qs-square qs-num Mon "><span class="qs-num">7</span></div><div class="qs-square qs-num Tue "><span class="qs-num">8</span></div><div class="qs-square qs-num Wed "><span class="qs-num">9</span></div><div class="qs-square qs-num Thu "><span class="qs-num">10</span></div><div class="qs-square qs-num Fri "><span class="qs-num">11</span></div><div class="qs-square qs-num Sat "><span class="qs-num">12</span></div><div class="qs-square qs-num Sun "><span class="qs-num">13</span></div><div class="qs-square qs-num Mon "><span class="qs-num">14</span></div><div class="qs-square qs-num Tue "><span class="qs-num">15</span></div><div class="qs-square qs-num Wed "><span class="qs-num">16</span></div><div class="qs-square qs-num Thu "><span class="qs-num">17</span></div><div class="qs-square qs-num Fri "><span class="qs-num">18</span></div><div class="qs-square qs-num Sat "><span class="qs-num">19</span></div><div class="qs-square qs-num Sun "><span class="qs-num">20</span></div><div class="qs-square qs-num Mon "><span class="qs-num">21</span></div><div class="qs-square qs-num Tue "><span class="qs-num">22</span></div><div class="qs-square qs-num Wed "><span class="qs-num">23</span></div><div class="qs-square qs-num Thu "><span class="qs-num">24</span></div><div class="qs-square qs-num Fri "><span class="qs-num">25</span></div><div class="qs-square qs-num Sat qs-current"><span class="qs-num">26</span></div><div class="qs-square qs-num Sun "><span class="qs-num">27</span></div><div class="qs-square qs-num Mon "><span class="qs-num">28</span></div><div class="qs-square qs-num Tue "><span class="qs-num">29</span></div><div class="qs-square qs-num Wed "><span class="qs-num">30</span></div><div class="qs-square qs-num Thu "><span class="qs-num">31</span></div><div class="qs-square qs-num Fri qs-empty"></div><div class="qs-square qs-num Sat qs-empty"></div></div>
+                                        <div class="qs-overlay qs-hidden">
+                                            <div class="qs-close">✕</div>
+                                            <input type="number" class="qs-overlay-year" placeholder="4-digit year">
+                                            <div class="qs-submit qs-disabled">Submit</div>
+                                        </div>
+                                    </div></div>
+                            </div>
+                            <div class="form-row tm-search-form-row">
+                                <div class="form-group tm-form-element tm-form-element-2">
+                                    <select name="adult" class="form-control tm-select" id="adult">
+                                        <option value="">Adult</option>
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                        <option value="5">5</option>
+                                        <option value="6">6</option>
+                                        <option value="7">7</option>
+                                        <option value="8">8</option>
+                                        <option value="9">9</option>
+                                        <option value="10">10</option>
+                                    </select>
+                                    <i class="fa fa-2x fa-user tm-form-element-icon"></i>
+                                </div>
+                                <div class="form-group tm-form-element tm-form-element-2">
+                                    <select name="children" class="form-control tm-select" id="children">
+                                        <option value="">Children</option>
+                                        <option value="0">0</option>
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                        <option value="5">5</option>
+                                        <option value="6">6</option>
+                                        <option value="7">7</option>
+                                        <option value="8">8</option>
+                                        <option value="9">9</option>
+                                        <option value="10">10</option>
+                                    </select>
+                                    <i class="fa fa-user tm-form-element-icon tm-form-element-icon-small"></i>
+                                </div>
+                                <div class="form-group tm-form-element tm-form-element-2">
+                                    <select name="room" class="form-control tm-select" id="room">
+                                        <option value="">Room</option>
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                        <option value="5">5</option>
+                                        <option value="6">6</option>
+                                        <option value="7">7</option>
+                                        <option value="8">8</option>
+                                        <option value="9">9</option>
+                                        <option value="10">10</option>
+                                    </select>
+                                    <i class="fa fa-2x fa-bed tm-form-element-icon"></i>
+                                </div>
+                                <div class="form-group tm-form-element tm-form-element-2">
+                                    <button type="submit" class="btn btn-primary tm-btn-search">Check Availability</button>
+                                </div>
+                            </div>
+                            <div class="form-row clearfix pl-2 pr-2 tm-fx-col-xs">
+                                <p class="tm-margin-b-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                <a href="#" class="ie-10-ml-auto ml-auto mt-1 tm-font-semibold tm-color-primary">Need Help?</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+
+
+        <? HOTEL SEARCH PART
+        check availability
+        ?>
+
+
     </div>
 </div>
+
+
--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -3,19 +3,27 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <meta name="author" content="">
     <title>@yield('title')</title>
-
     <meta name="description" content="@yield('description')">
     <meta name="keywords" content="@yield('keywords')">
-
     <!-- Bootstrap core CSS -->
     <link href="{{asset('assets')}}/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <!-- Fontawesome CSS -->
     <link href="{{asset('assets')}}/css/all.css" rel="stylesheet">
     <!-- Custom styles for this template -->
     <link href="{{asset('assets')}}/css/style.css" rel="stylesheet">
+
+    <? check availability  ?>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">  <!-- Google web font "Open Sans" -->
+    <link rel="stylesheet" href="{{asset('assets')}}/font-awesome-4.7.0/css/font-awesome.min.css">                <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{asset('assets')}}/css/bootstrap.min.css">                                      <!-- Bootstrap style -->
+    <link rel="stylesheet" type="text/css" href="{{asset('assets')}}/slick/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="{{asset('assets')}}/slick/slick-theme.css"/>
+    <link rel="stylesheet" type="text/css" href="{{asset('assets')}}/css/datepicker.css"/>
+    <link rel="stylesheet" href="{{asset('assets')}}/css/tooplate-style.css">
+    <? check availability  ?>
+
     @yield('css')
     @yield('headerjs')
 </head>
@@ -32,6 +40,152 @@
 <!-- Bootstrap core JavaScript -->
 <script src="{{asset('assets')}}/vendor/jquery/jquery.min.js"></script>
 <script src="{{asset('assets')}}/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<? check availability  ?>
+<script src="{{asset('assets')}}/js/jquery-1.11.3.min.js"></script>             <!-- jQuery (https://jquery.com/download/) -->
+<script src="{{asset('assets')}}/js/popper.min.js"></script>                    <!-- https://popper.js.org/ -->
+<script src="{{asset('assets')}}/js/bootstrap.min.js"></script>                 <!-- https://getbootstrap.com/ -->
+<script src="{{asset('assets')}}/js/datepicker.min.js"></script>                <!-- https://github.com/qodesmith/datepicker -->
+<script src="{{asset('assets')}}/js/jquery.singlePageNav.min.js"></script>      <!-- Single Page Nav (https://github.com/ChrisWojcik/single-page-nav) -->
+<script src="{{asset('assets')}}/slick/slick.min.js"></script>                  <!-- http://kenwheeler.github.io/slick/ -->
+<script>
+
+    /* Google map
+    ------------------------------------------------*/
+    var map = '';
+    var center;
+
+    function initialize() {
+        var mapOptions = {
+            zoom: 16,
+            center: new google.maps.LatLng(13.7567928,100.5653741),
+            scrollwheel: false
+        };
+
+        map = new google.maps.Map(document.getElementById('google-map'),  mapOptions);
+
+        google.maps.event.addDomListener(map, 'idle', function() {
+            calculateCenter();
+        });
+
+        google.maps.event.addDomListener(window, 'resize', function() {
+            map.setCenter(center);
+        });
+    }
+
+    function calculateCenter() {
+        center = map.getCenter();
+    }
+
+    function loadGoogleMap(){
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDVWt4rJfibfsEDvcuaChUaZRS5NXey1Cs&v=3.exp&sensor=false&' + 'callback=initialize';
+        document.body.appendChild(script);
+    }
+
+    function setCarousel() {
+
+        if ($('.tm-article-carousel').hasClass('slick-initialized')) {
+            $('.tm-article-carousel').slick('destroy');
+        }
+
+        if($(window).width() < 438){
+            // Slick carousel
+            $('.tm-article-carousel').slick({
+                infinite: false,
+                dots: true,
+                slidesToShow: 1,
+                slidesToScroll: 1
+            });
+        }
+        else {
+            $('.tm-article-carousel').slick({
+                infinite: false,
+                dots: true,
+                slidesToShow: 2,
+                slidesToScroll: 1
+            });
+        }
+    }
+
+    function setPageNav(){
+        if($(window).width() > 991) {
+            $('#tm-top-bar').singlePageNav({
+                currentClass:'active',
+                offset: 79
+            });
+        }
+        else {
+            $('#tm-top-bar').singlePageNav({
+                currentClass:'active',
+                offset: 65
+            });
+        }
+    }
+
+    function togglePlayPause() {
+        vid = $('.tmVideo').get(0);
+
+        if(vid.paused) {
+            vid.play();
+            $('.tm-btn-play').hide();
+            $('.tm-btn-pause').show();
+        }
+        else {
+            vid.pause();
+            $('.tm-btn-play').show();
+            $('.tm-btn-pause').hide();
+        }
+    }
+
+    $(document).ready(function(){
+
+        $(window).on("scroll", function() {
+            if($(window).scrollTop() > 100) {
+                $(".tm-top-bar").addClass("active");
+            } else {
+                //remove the background property so it comes transparent again (defined in your css)
+                $(".tm-top-bar").removeClass("active");
+            }
+        });
+
+        // Google Map
+        loadGoogleMap();
+
+        // Date Picker
+        const pickerCheckIn = datepicker('#inputCheckIn');
+        const pickerCheckOut = datepicker('#inputCheckOut');
+
+        // Slick carousel
+        setCarousel();
+        setPageNav();
+
+        $(window).resize(function() {
+            setCarousel();
+            setPageNav();
+        });
+
+        // Close navbar after clicked
+        $('.nav-link').click(function(){
+            $('#mainNav').removeClass('show');
+        });
+
+        // Control video
+        $('.tm-btn-play').click(function() {
+            togglePlayPause();
+        });
+
+        $('.tm-btn-pause').click(function() {
+            togglePlayPause();
+        });
+
+        // Update the current year in copyright
+        $('.tm-current-year').text(new Date().getFullYear());
+    });
+
+</script>
+<? check availability  ?>
 
 @yield('footerjs')
 
